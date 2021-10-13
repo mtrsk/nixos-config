ce2e26a01c1ee03ca770e8581989471f364f5208
Marcos Benevides <marcos.schonfinkel@gmail.com>
2021-10-12 23:19:28 -0300

    chore: change emacs config

--- dotfiles/emacs.d/roam.el
+++ dotfiles/emacs.d/roam.el
@@ -1,15 +1,16 @@
 (use-package org-roam
-      :ensure t
-      :custom
-      (org-roam-directory (file-truename "~/Notes"))
-      :bind (("C-c n l" . org-roam-buffer-toggle)
-             ("C-c n f" . org-roam-node-find)
-             ("C-c n g" . org-roam-graph)
-             ("C-c n i" . org-roam-node-insert)
-             ("C-c n c" . org-roam-capture)
-             ;; Dailies
-             ("C-c n j" . org-roam-dailies-capture-today))
-      :config
-      (org-roam-setup))
-
-(setq org-roam-v2-ack t)
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory (file-truename "~/Notes"))
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         :map org-mode-map
+         ("C-M-i"   . completion-at-point))
+  :config
+  (org-roam-setup))
