03093fa0778f5575babfc1a04b273ff342c8216a
Marcos Benevides <marcos.schonfinkel@gmail.com>
2022-07-06 10:00:52 -0300

    chore: update sway

--- services/swaywm.nix
+++ services/swaywm.nix
@@ -98,11 +98,11 @@ in
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    # gtk portal needed to make gtk apps happy
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
-    gtkUsePortal = true;
+  #  # gtk portal needed to make gtk apps happy
+  #  extraPortals = with pkgs; [
+  #    xdg-desktop-portal-gtk
+  #  ];
+  #  gtkUsePortal = true;
   };
 
   programs.waybar.enable = true;
