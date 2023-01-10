58d50d38a0e096371dd3e9cdd4d359f0d4cd04ca
Marcos Benevides <marcos.schonfinkel@gmail.com>
2023-01-10 00:12:20 -0300

    nix: update flakes

--- hosts/arrakis/configuration.nix
+++ hosts/arrakis/configuration.nix
@@ -75,6 +75,11 @@ in
 
   console.useXkbConfig = true;
 
+  # Until I fix screen sharing
+  #services.xserver.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+
   services.xserver = {
     layout = kbdLayout;
     xkbOptions = "ctrl:nocaps";
