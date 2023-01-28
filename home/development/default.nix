e86f5211d00d52b97b1ff5569202ec1fbfda6b2e
Marcos Benevides <marcos.schonfinkel@gmail.com>
2023-01-27 22:13:47 -0300

    nix: finish workstation setup

--- home/development/default.nix
+++ home/development/default.nix
@@ -29,6 +29,9 @@
     defaultCacheTtlSsh = 34560000;
     maxCacheTtl = 34560000;
     maxCacheTtlSsh = 34560000;
+    sshKeys = [
+      "23C94318A1D57DA26574677539EC504CB2A49981"
+    ];
   };
 
   services.lorri.enable = true;
