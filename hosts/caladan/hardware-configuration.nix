32e84eb885484af36e4ef055e112b58152016d52
Marcos Benevides <marcos.schonfinkel@gmail.com>
2023-03-25 13:06:48 -0300

    nix: update host

--- hosts/caladan/hardware-configuration.nix
+++ hosts/caladan/hardware-configuration.nix
@@ -39,6 +39,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  # high-resolution display
-  fonts.optimizeForVeryHighDPI = lib.mkDefault true;
 }
