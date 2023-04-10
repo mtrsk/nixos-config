44e99d37dbacd25f499ae0e0eaf1d49537435a53
Marcos Benevides <marcos.schonfinkel@gmail.com>
2023-04-09 23:24:19 -0300

    add noisetorch

--- hosts/caladan/configuration.nix
+++ hosts/caladan/configuration.nix
@@ -106,6 +106,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.noisetorch.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
