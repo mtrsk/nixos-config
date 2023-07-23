7aba9411f508993c5ea29a919cb0b7cd9951e869
Marcos Benevides <marcos.schonfinkel@gmail.com>
2023-07-23 10:56:25 -0300

    nix: update flakes

--- documentation.nix
+++ documentation.nix
@@ -2,11 +2,11 @@
 
 {
   documentation = {
-    enable       = true;
-    dev.enable   = true;
-    doc.enable   = true;
-    info.enable  = true;
-    man.enable   = true;
-    nixos.enable = true;
+    enable       = false;
+    #dev.enable   = true;
+    #doc.enable   = true;
+    #info.enable  = true;
+    #man.enable   = true;
+    #nixos.enable = true;
   };
 }
