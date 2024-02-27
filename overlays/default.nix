2f099e6b8c103437fdf11af125e543c9307f8141
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-02-27 19:55:03 -0300

    nix: update postman

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,11 +9,10 @@
     #eww
     (final: prev: {
       postman = prev.postman.overrideAttrs(old: rec {
-        version = "20230716100528";
+        version = "20231205182607";
         src = final.fetchurl {
           url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
-          sha256 = "sha256-svk60K4pZh0qRdx9+5OUTu0xgGXMhqvQTGTcmqBOMq8=";
-
+          sha256 = "sha256-PthETmSLehg6eWpdDihH1juwiyZdJvzr+qyG2YYuEZI=";
           name = "${old.pname}-${version}.tar.gz";
         };
       });
