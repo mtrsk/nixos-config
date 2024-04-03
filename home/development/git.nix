605ece9e93746a364ff98e339c288e702a67f9c4
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-04-03 20:25:50 -0300

    nix: improve git

--- home/development/git.nix
+++ home/development/git.nix
@@ -1,5 +1,12 @@
 {pkgs, ...}:
 
+let
+  main = {
+    email = "marcos.schonfinkel@gmail.com";
+    name = "Marcos Benevides";
+    signingKey = "5E35E6094143FA7A";
+  };
+in
 {
   home.packages = with pkgs; [
     bfg-repo-cleaner
@@ -15,15 +22,24 @@
     userName = "Marcos Benevides";
 
     aliases = {
+      # Aliases
       commit = "commit -S";
       a = "add";
       ap = "add -p";
-      c = "commit -m";
+      c = "clone";
+      ca = "commit --amend";
+      can = "commit --amend --no-edit";
+      cm = "commit -m";
+      ds = "diff --staged";
+      dc = "diff --name-only --diff-filter=U";
       po = "push origin";
       pof = "push -f origin";
       pro = "pull --rebase origin";
-      list-staged = "diff --staged";
-      list-conflicts = "diff --name-only --diff-filter=U";
+      ra = "rebase --abort";
+      rc = "rebase --continue";
+      ri = "rebase -i";
+      # Pretty
+      branches = "branch --sort=-committerdate --format='%(HEAD)%(color:yellow) %(refname:short) | %(color:bold red)%(committername) | %(color:bold green)%(committerdate:relative) | %(color:blue)%(subject)%(color:reset)' --color=always";
     };
 
     delta = {
@@ -44,11 +60,19 @@
       };
 
       safe = {
-        directory = "/home/leto/NixOS";
+        directory = "~/Code/NixOS";
       };
     };
 
     includes = [
+      {
+        condition = "gitdir:~/Code/";
+        contents.user = main;
+        commit = {
+          gpgSign = true;
+        };
+      }
+
       {
         condition = "gitdir:~/Work/";
         path = "~/Work/.gitconfig";
@@ -61,7 +85,10 @@
 
       {
         condition = "gitdir:~/.password-store/";
-        path = "~/.password-store/.gitconfig";
+        contents.user = main;
+        commit = {
+          gpgSign = true;
+        };
       }
     ];
   };
