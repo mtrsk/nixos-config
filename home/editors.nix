b825f50904f8ba218438d8306329f17572bfa881
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-05-08 12:19:18 -0300

    nix: add proper vscodium config

--- home/editors.nix
+++ home/editors.nix
@@ -1,11 +1,91 @@
 { pkgs, lib, config, ... }:
 
+let
+  inherit (pkgs)
+    nixpkgs-fmt
+    nil
+    ;
+in
 {
   imports =
     [
       ./neovim
     ];
 
-  home.packages = with pkgs; [ vscode ];
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    userSettings = {
+      "update.mode" = "none";
+
+      "editor.formatOnSave" = false;
+      "editor.linkedEditing" = true;
+      "editor.rulers" = [ 80 120 ];
+
+      # excluded files
+      "files.exclude" = {
+        # removes these from the search
+        "**/.direnv" = true;
+        "**/.devenv" = true;
+      };
+
+      "workbench.tree.indent" = 15;
+      "workbench.colorTheme" = "Default Dark Modern";
+
+      "terminal.integrated.tabs.enabled" = true;
+
+      "window.titleBarStyle" = "custom";
+      "window.zoomLevel" = 0;
+
+      # F#
+      "FSharp.inlayHints.enabled" = false;
+      "FSharp.inlayHints.typeAnnotations" = false;
+      "FSharp.inlayHints.parameterNames" = false;
+
+      # HTML
+      "[html]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+
+      # Nix
+      "nix" = {
+        "enableLanguageServer" = true;
+        "formatterPath" = "${nixpkgs-fmt}/bin/nixpkgs-fmt";
+        "serverPath" = "${nil}/bin/nil";
+      };
+      "[nix]" = {
+        "editor.insertSpaces" = true;
+        "editor.tabSize" = 2;
+      };
+    };
+
+    extensions = with pkgs.vscode-extensions; [
+      # .NET
+      ionide.ionide-fsharp
+      ms-dotnettools.csharp
+
+      # Nix
+      jnoortheen.nix-ide
+
+      # Python
+      ms-python.python
+      ms-toolsai.jupyter
+      ms-pyright.pyright
+
+      # Markdown
+      yzhang.markdown-all-in-one
+
+      # Misc
+      eamodio.gitlens
+      editorconfig.editorconfig
+      esbenp.prettier-vscode
+      gruntfuggly.todo-tree
+      mkhl.direnv
+      vscodevim.vim
+    ];
+  };
+
+  programs.vscode.userSettings.editor.fontFamily = "Jetbrains Mono";
+  home.packages = with pkgs; [ jetbrains-mono ];
 }
 
