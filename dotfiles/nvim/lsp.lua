d2055fde9a0b6c85efcdf23831143d39a736daf0
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-09-07 21:00:57 -0300

    nix: format neovim config

--- dotfiles/nvim/lsp.lua
+++ dotfiles/nvim/lsp.lua
@@ -16,12 +16,14 @@ _G.tab_complete = function()
     end
 end
 
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+-- Autoformat on save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 -- Setup
-require "lspconfig"
+local lspconfig = require('lspconfig')
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -29,7 +31,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 -- Elixir
 -- Every Elixir devenv needs to have this envar defined
 local elixir_ls_path = os.getenv("ELIXIR_LS_PATH")
-require 'lspconfig'.elixirls.setup {
+lspconfig.elixirls.setup {
     cmd = { elixir_ls_path },
     filetypes = { "elixir", "eelixir", "heex", "surface" },
     capabilities = capabilities,
@@ -37,7 +39,7 @@ require 'lspconfig'.elixirls.setup {
 }
 
 -- Erlang
-require 'lspconfig'.erlangls.setup{
+require 'lspconfig'.erlangls.setup {
     capabilities = capabilities,
 }
 
@@ -62,61 +64,72 @@ vim.g["fsharp#show_signature_on_cursor_move"] = 1
 vim.g["fsharp#fsi_focus_on_send"] = 1
 
 -- Gleam
-require 'lspconfig'.gleam.setup({
-	cmd = { "gleam", "lsp" },
-	filetypes = { "gleam" },
-    capabilities = capabilities
+lspconfig.gleam.setup({
+    --cmd = { "gleam", "lsp" },
+    --filetypes = { "gleam" },
+    --capabilities = capabilities
 })
 
 -- Just
 require("nvim-treesitter.configs").setup({
-  highlight = {
-    enable = true,
-    disable = { "just" },
-  },
+    highlight = {
+        enable = true,
+        disable = { "just" },
+    },
+    indent = { enable = true },
 })
 
 -- Nix
-require'lspconfig'.nil_ls.setup{}
+lspconfig.nil_ls.setup({
+    capabilities = capabilities,
+    settings = {
+        ['nil'] = {
+            formatting = {
+                command = { "nixfmt" },
+            },
+        },
+    },
+})
 
 -- Terraform
-require'lspconfig'.terraformls.setup{}
+require 'lspconfig'.terraformls.setup {}
 vim.g.terraform_fmt_on_save = 1
 vim.g.terraform_align = 1
 
 -- Lua
-require'lspconfig'.lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+require 'lspconfig'.lua_ls.setup {
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-  },
 }
 
 -----------------------
 -- Autocompletion setup
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-        require('luasnip').lsp_expand(args.body)
+            require('luasnip').lsp_expand(args.body)
         end,
     },
     window = {
@@ -124,17 +137,17 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     formatting = {
-        fields = {'menu', 'abbr', 'kind'},
+        fields = { 'menu', 'abbr', 'kind' },
         format = function(entry, item)
-        local menu_icon = {
-            nvim_lsp = 'λ',
-            luasnip = '⋗',
-            buffer = 'Ω',
-            path = '🖫',
-        }
+            local menu_icon = {
+                nvim_lsp = 'λ',
+                luasnip = '⋗',
+                buffer = 'Ω',
+                path = '🖫',
+            }
 
-        item.menu = menu_icon[entry.source.name]
-        return item
+            item.menu = menu_icon[entry.source.name]
+            return item
         end,
     },
     mapping = {
@@ -148,8 +161,8 @@ cmp.setup({
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-y>'] = cmp.mapping.confirm({select = true}),
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
         ['<C-f>'] = cmp.mapping(function(fallback)
             if luasnip.jumpable(1) then
@@ -157,7 +170,7 @@ cmp.setup({
             else
                 fallback()
             end
-            end, {'i', 's'}
+        end, { 'i', 's' }
         ),
         ['<C-b>'] = cmp.mapping(function(fallback)
             if luasnip.jumpable(-1) then
@@ -165,7 +178,7 @@ cmp.setup({
             else
                 fallback()
             end
-            end, {'i', 's'}
+        end, { 'i', 's' }
         ),
         ['<Tab>'] = cmp.mapping(function(fallback)
             local col = vim.fn.col('.') - 1
@@ -177,7 +190,7 @@ cmp.setup({
             else
                 cmp.complete()
             end
-            end, {'i', 's'}
+        end, { 'i', 's' }
         ),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -185,22 +198,22 @@ cmp.setup({
             else
                 fallback()
             end
-            end, {'i', 's'}
+        end, { 'i', 's' }
         ),
     },
     sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-            { name = 'treesitter' }
-        }, {
-            { name = 'buffer' },
-        })
-    }
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'treesitter' }
+    }, {
+        { name = 'buffer' },
+    })
+}
 )
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-sources = cmp.config.sources({
+    sources = cmp.config.sources({
         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
     }, {
         { name = 'buffer' },
@@ -219,65 +232,57 @@ cmp.setup.cmdline({ '/', '?' }, {
 cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-            { name = 'path' }
-        }, {
-            { name = 'cmdline' }
-        })
-    }
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+}
 )
 
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['nil_ls'].setup {
-    capabilities = capabilities
-}
-
-require('lspconfig')['lua_ls'].setup {
-    capabilities = capabilities
-}
 
 -- Keybindings
 vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = {buffer = true}
-      vim.keymap.set(mode, lhs, rhs, opts)
+    desc = 'LSP actions',
+    callback = function()
+        local bufmap = function(mode, lhs, rhs)
+            local opts = { buffer = true }
+            vim.keymap.set(mode, lhs, rhs, opts)
+        end
+
+        -- Displays hover information about the symbol under the cursor
+        bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+        -- Jump to the definition
+        bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+        -- Jump to declaration
+        bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+        -- Lists all the implementations for the symbol under the cursor
+        bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+        -- Jumps to the definition of the type symbol
+        bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+        -- Lists all the references
+        bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+        -- Displays a function's signature information
+        bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+        -- Renames all references to the symbol under the cursor
+        bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+        -- Selects a code action available at the current cursor position
+        bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+
+        -- Show diagnostics in a floating window
+        bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+        -- Move to the previous diagnostic
+        bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+        -- Move to the next diagnostic
+        bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
     end
-
-    -- Displays hover information about the symbol under the cursor
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-
-    -- Jump to the definition
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-
-    -- Jump to declaration
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-
-    -- Lists all the implementations for the symbol under the cursor
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-
-    -- Jumps to the definition of the type symbol
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-
-    -- Lists all the references 
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-
-    -- Displays a function's signature information
-    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-
-    -- Renames all references to the symbol under the cursor
-    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-
-    -- Selects a code action available at the current cursor position
-    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-
-    -- Show diagnostics in a floating window
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-
-    -- Move to the previous diagnostic
-    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-
-    -- Move to the next diagnostic
-    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  end
 })
